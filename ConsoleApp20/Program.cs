1fb536d44bfd44aea96dbbed29b602e24614cc87
Ahtyamov <Ahtyamov@WIN-SFUQ8BAFB06>
2023-01-25 10:35:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.WriteLine("я программа");
+
+while (true)
+{
+    Console.WriteLine("Список команд:");
+    Console.WriteLine("Регистрация -  \"add user\"");
+    Console.WriteLine("Вход в  систему -  \"in\"");
+    Console.WriteLine("Выход -  \"exit\"");
+    Console.WriteLine("очистить консоль -  \"clear\"");
+
+    string com = Console.ReadLine();
+
+    switch (com)
+    {
+        case "add user": AddUser(); break; ;
+        case "in": AutUser(); break;
+        case "clear": Console.Clear(); break;
+        case "exit": return; ;
+        default: Console.WriteLine("команда не  ясна - попробуйте  еще раз");
+            break;
+    }
+}
+
+void AutUser()
+{
+    throw new NotImplementedException();
+}
+
+void AddUser()
+{
+    throw new NotImplementedException();
+}
